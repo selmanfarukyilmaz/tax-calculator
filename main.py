e442efd281b1073ef0c7da25fc24c2d77ff7d72d
selman <aksamavisi@gmail.com>
2022-05-10 18:46:51 +0300

    plot funcs Created yaml file re-Created

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 
 import yaml
 import unittest
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy
 
 
 def calculate_tax(income: int, selected_country_year: str) -> float:
@@ -14,15 +17,24 @@ def calculate_tax(income: int, selected_country_year: str) -> float:
     # print("Please enter your salary")
     # salary = int(input())
     tax_rates = []
+    # print(config)
+    # for tax_year in config:
+    #     if tax_year == selected_country_year:
+    #
+    #         for tax_rate_money in config[tax_year]:
+    #             if tax_rate_money == "None":
+    #                 tax_rates.append((None, config[tax_year][tax_rate_money]))
+    #                 continue
+    #             tax_rates.append((tax_rate_money, config[tax_year][tax_rate_money]))
 
-    for tax_year in config:
-        if tax_year == selected_country_year:
-
-            for tax_rate_money in config[tax_year]:
-                if tax_rate_money == "None":
-                    tax_rates.append((None, config[tax_year][tax_rate_money]))
-                    continue
-                tax_rates.append((tax_rate_money, config[tax_year][tax_rate_money]))
+    if config[selected_country_year]:
+        tax_rates = config[selected_country_year]
+        for index, rates in enumerate(tax_rates):
+            for value in rates:
+                if value == "None":
+                    tax_rates[index][0] = None
+    else:
+        raise Exception("tr2021 or tr2022 or uk2022 available")
 
     tax = 0
     band = 0
@@ -44,6 +56,61 @@ def calculate_tax(income: int, selected_country_year: str) -> float:
 # income = 500000
 # year_country = "tr2022"
 # print(f"tax: {calculate_tax(income=income, selected_country_year=year_country)}")
+
+def plot_country(selected_country_year: str, zero_to_where: int):
+    results = []
+    values = list(range(0, zero_to_where, 250))
+
+    for i in values:
+        results.append(calculate_tax(i, selected_country_year))
+        print(results[:-1])
+
+    plt.plot(values, results, label='money-tax')
+    plt.autoscale(tight=True)
+    plt.title(selected_country_year)
+    plt.legend(loc=0)
+    plt.ylabel('Tax')
+    plt.xlabel('Money')
+    plt.ticklabel_format(style='plain')
+
+    plt.show()
+
+
+plot_country("uk2022", 1200000)
+
+
+def plot_all(zero_to_where: int):
+    values = list(range(0, zero_to_where, 250))
+
+    results_uk2022 = []
+    for i in values:
+        results_uk2022.append(calculate_tax(i, "uk2022"))
+    print("1/3 finish")
+
+    results_tr2021 = []
+    for i in values:
+        results_tr2021.append(calculate_tax(i, "tr2021"))
+    print("2/3 finish")
+
+    results_tr2022 = []
+    for i in values:
+        results_tr2022.append(calculate_tax(i, "tr2022"))
+    print("3/3 finish")
+
+    plt.plot(values, results_uk2022, label="uk2022")
+    plt.plot(values, results_tr2021, label="tr2021")
+    plt.plot(values, results_tr2022, label="tr2022")
+    plt.autoscale(tight=True)
+    plt.title("TAX-MONEY")
+    plt.legend(loc=0)
+    plt.ylabel('Tax')
+    plt.xlabel('Money')
+    plt.ticklabel_format(style='plain')
+
+    plt.show()
+
+
+plot_all(1200000)
 
 
 class TestStringMethods(unittest.TestCase):
